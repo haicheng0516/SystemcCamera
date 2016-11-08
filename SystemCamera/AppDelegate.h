25e3a3b6b032ec288d0c7c922d43d4a412366085
haicheng0516 <haicheng0516@163.com>
2016-11-09 00:19:12 +0800

    Initial Commit

---
+++ SystemCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SystemCamera
+//
+//  Created by 郭健 on 2016/11/9.
+//  Copyright © 2016年 海城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
